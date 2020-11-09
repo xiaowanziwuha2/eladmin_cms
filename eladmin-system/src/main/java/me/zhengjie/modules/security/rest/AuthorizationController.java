b57f6728778a28a2f841b1fe6395003559faf18e
xiaowanziwuha2 <89444444@163.com>
2020-11-09 11:38:39 +0800

    update logback, and loginController

--- eladmin-system/src/main/java/me/zhengjie/modules/security/rest/AuthorizationController.java
+++ eladmin-system/src/main/java/me/zhengjie/modules/security/rest/AuthorizationController.java
@@ -76,17 +76,17 @@ public class AuthorizationController {
     @AnonymousPostMapping(value = "/login")
     public ResponseEntity<Object> login(@Validated @RequestBody AuthUserDto authUser, HttpServletRequest request) throws Exception {
         // 密码解密
-        String password = RsaUtils.decryptByPrivateKey(RsaProperties.privateKey, authUser.getPassword());
+        String password = authUser.getPassword();//RsaUtils.decryptByPrivateKey(RsaProperties.privateKey, authUser.getPassword());
         // 查询验证码
-        String code = (String) redisUtils.get(authUser.getUuid());
+        //String code = (String) redisUtils.get(authUser.getUuid());
         // 清除验证码
-        redisUtils.del(authUser.getUuid());
-        if (StringUtils.isBlank(code)) {
+        //redisUtils.del(authUser.getUuid());
+        /*if (StringUtils.isBlank(code)) {
             throw new BadRequestException("验证码不存在或已过期");
-        }
-        if (StringUtils.isBlank(authUser.getCode()) || !authUser.getCode().equalsIgnoreCase(code)) {
+        }*/
+        /*if (StringUtils.isBlank(authUser.getCode()) || !authUser.getCode().equalsIgnoreCase(code)) {
             throw new BadRequestException("验证码错误");
-        }
+        }*/
         UsernamePasswordAuthenticationToken authenticationToken =
                 new UsernamePasswordAuthenticationToken(authUser.getUsername(), password);
         Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
